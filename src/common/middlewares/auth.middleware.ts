053e234e809043ae55109791e38f8367baf8a839
Khoa Pham Duy Dang <blackparadise0407@gmail.com>
2022-09-20 13:26:28 +0700

    Add permission checking middleware

--- src/common/middlewares/auth.middleware.ts
+++ src/common/middlewares/auth.middleware.ts
@@ -1,8 +1,29 @@
+import { RequestHandler } from 'express';
 import { auth } from 'express-oauth2-jwt-bearer';
 
 import { ENV } from '@/constants';
+
+import { ForbiddenException } from '../exceptions/ForbiddenException';
 
 export const checkJwt = auth({
   audience: ENV.AUDIENCE,
   issuerBaseURL: ENV.ISSUER_BASE_URL,
 });
+
+export const checkPermissions =
+  (permissions: string | string[]): RequestHandler =>
+  (req, _, next) => {
+    const perms = (req.auth?.payload.permissions ?? []) as string[];
+    if (
+      perms.some((perm) =>
+        (typeof permissions === 'string'
+          ? [permissions]
+          : permissions
+        ).includes(perm),
+      )
+    ) {
+      next();
+    } else {
+      next(new ForbiddenException());
+    }
+  };
--- src/routes/transaction.route.ts
+++ src/routes/transaction.route.ts
@@ -1,10 +1,15 @@
 import express from 'express';
 
+import { checkPermissions } from '@/common/middlewares/auth.middleware';
 import { transactionController } from '@/controllers/transaction.controller';
 
 const router = express.Router();
 
-router.get('/', transactionController.get);
+router.get(
+  '/',
+  checkPermissions('read:transactions'),
+  transactionController.get,
+);
 router.post('/', transactionController.create);
 router.get('/analytics', transactionController.getAnalytic);
 router.delete('/:id', transactionController.delete);
