b6fde068dce4c31b5dceb79e47e541b12d9a41f5
blackparadise0407 <blackparadise0407@gmail.com>
2022-10-09 20:09:33 +0700

    Remove permission check

--- src/routes/transaction.route.ts
+++ src/routes/transaction.route.ts
@@ -1,15 +1,10 @@
 import express from 'express';
 
-import { checkPermissions } from '@/common/middlewares/auth.middleware';
 import { transactionController } from '@/controllers/transaction.controller';
 
 const router = express.Router();
 
-router.get(
-  '/',
-  checkPermissions('read:transactions'),
-  transactionController.get,
-);
+router.get('/', transactionController.get);
 router.post('/', transactionController.create);
 router.get('/analytics', transactionController.getAnalytic);
 router.delete('/:id', transactionController.delete);
